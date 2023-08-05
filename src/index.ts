7e1520c6d975b6ce2cee75facfb0d2d6227ba33e
Florian Pallas <mail@fpallas.com>
2023-08-05 02:53:28 +0200

    provide mz export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export const mz = {};
+import * as types from "./types";
+export * from "./types";
+
+import * as convert from "./convert";
+export * from "./convert";
+
+export const mz = {
+  ...types,
+  ...convert,
+};
